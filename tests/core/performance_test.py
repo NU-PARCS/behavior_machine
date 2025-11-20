4380f3626703b32285b58c27f407b5bb038396f3
Zhi Tan <zhi.tan@northeastern.edu>
2025-11-19 22:36:45 -0500

    changed the test parameters.

--- tests/core/performance_test.py
+++ tests/core/performance_test.py
@@ -22,13 +22,13 @@ def test_repeat_node_in_machine_fast():
     ds2.add_transition_on_success(ds3)
     ds3.add_transition_on_success(ds1)
 
-    exe = Machine(ds1, rate=60)
+    exe = Machine(ds1, rate=30)
     exe.start(None)
     time.sleep(2)
     exe.interrupt()
     # the performance of the computer might change this.
-    assert counter >= (60 * 2) - 2
-    assert counter <= (60 * 2) + 1
+    assert counter >= (30 * 2) * 0.9
+    assert counter <= (30 * 2) * 1.1
 
 def test_validate_transition_immediate():
 
@@ -47,13 +47,13 @@ def test_validate_transition_immediate():
     ds2.add_transition(lambda s, b: True, ds3)
     ds3.add_transition(lambda s, b: True, ds1)
 
-    exe = Machine(ds1, rate=60)
+    exe = Machine(ds1, rate=30)
     exe.start(None)
     time.sleep(2)
     exe.interrupt()
     # the performance of the computer might change this.
-    assert counter >= (60 * 2) - 2
-    assert counter <= (60 * 2) + 1
+    assert counter >= (30 * 2) * 0.9
+    assert counter <= (30 * 2) * 1.1
 
 
 def test_multiple_parallel_states():
@@ -62,7 +62,7 @@ def test_multiple_parallel_states():
         def execute(self, board: Board) -> StateStatus:
             return StateStatus.SUCCESS
 
-    num_parallel = 500
+    num_parallel = 1000
     child_states = []
     for i in range(0, num_parallel):
         child_states.append(CompleteState(f"I{i}"))
